a2756d9ab2c7986c3304597ea2f5ebc37ff2d1bf
James Ridgway <james@jamesridgway.co.uk>
2020-11-08 09:46:32 +0000

    Implemented microphone control

--- controls/mic_mute_control.py
+++ controls/mic_mute_control.py
@@ -1,16 +1,31 @@
 import os
 
+from pulsectl import pulsectl
+
 from controls.deck_control import DeckControl
 
 
 class MicMuteControl(DeckControl):
+
+    def __init__(self, **kwargs):
+        self.pulse = pulsectl.Pulse()
+        super().__init__(**kwargs)
+
     def initialize(self, control_context):
-        control_context.set_icon(os.path.join(os.path.dirname(__file__), "../assets", 'microphone.png'))
-        self.muted = False
+        self.__render_icon(control_context)
 
     def pressed(self, control_context):
-        self.muted = not self.muted
-        if self.muted:
-            control_context.set_icon(os.path.join(os.path.dirname(__file__), "../assets", 'microphone-mute.png'))
-        else:
+        mic = self.__get_mic()
+        self.pulse.source_mute(mic.index, mute=(not mic.mute))
+        self.__render_icon(control_context)
+
+    def __get_mic(self):
+        sources = self.pulse.source_list()
+        return [mic for mic in sources if mic.description == self.settings['microphone']][0]
+
+    def __render_icon(self, control_context):
+        mic = self.__get_mic()
+        if mic.mute == 0:
             control_context.set_icon(os.path.join(os.path.dirname(__file__), "../assets", 'microphone.png'))
+        else:
+            control_context.set_icon(os.path.join(os.path.dirname(__file__), "../assets", 'microphone-mute.png'))
