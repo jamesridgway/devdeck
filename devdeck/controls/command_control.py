4adb8196850493ed44a48e7688492e94d2b7170a
James Ridgway <james@jamesridgway.co.uk>
2021-07-03 11:39:35 +0100

    Improving error handling for command control

--- devdeck/controls/command_control.py
+++ devdeck/controls/command_control.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from subprocess import Popen, DEVNULL
 
@@ -5,10 +6,17 @@ from devdeck_core.controls.deck_control import DeckControl
 
 
 class CommandControl(DeckControl):
+    def __init__(self, key_no, **kwargs):
+        self.__logger = logging.getLogger('devdeck')
+        super().__init__(key_no, **kwargs)
+
     def initialize(self):
         with self.deck_context() as context:
             with context.renderer() as r:
                 r.image(os.path.expanduser(self.settings['icon'])).end()
 
     def pressed(self):
-        Popen(self.settings['command'], stdout=DEVNULL, stderr=DEVNULL)
+        try:
+            Popen(self.settings['command'], stdout=DEVNULL, stderr=DEVNULL)
+        except Exception as ex:
+            self.__logger.error("Error executing command %s: %s", self.settings['command'], str(ex))
